f0834b6280b18b838717efdcb2500f52defcbf74
mozarif <mohasher.zarif@gmail.com>
2026-01-08 17:35:52 +0100

    fixing indentation in the examples

--- examples/papi_migrate_cpus.cpp
+++ examples/papi_migrate_cpus.cpp
@@ -23,92 +23,92 @@
 
 void migrate(int targetCpu)
 {
-  cpu_set_t cpuSet;
-  CPU_ZERO(&cpuSet);
-  CPU_SET(targetCpu, &cpuSet);
+    cpu_set_t cpuSet;
+    CPU_ZERO(&cpuSet);
+    CPU_SET(targetCpu, &cpuSet);
 
-  if (pthread_setaffinity_np(pthread_self(), sizeof(cpuSet), &cpuSet))
-    exit(EXIT_FAILURE);
+    if (pthread_setaffinity_np(pthread_self(), sizeof(cpuSet), &cpuSet))
+        exit(EXIT_FAILURE);
 
-  sched_yield();
-  
-  int cpu = sched_getcpu();
-  assert(cpu == targetCpu);
+    sched_yield();
+    
+    int cpu = sched_getcpu();
+    assert(cpu == targetCpu);
 }
 
 void saxpy(double *a, const double *b, const double *c, size_t n, double alpha)
 {
-  for (size_t i = 0; i < n; i++)
-    a[i] = alpha * b[i] + c[i];
+    for (size_t i = 0; i < n; i++)
+        a[i] = alpha * b[i] + c[i];
 }
 
 int main(int argc, char **argv)
 {
-  if (argc != 2) {
-    std::cerr << "usage: " << argv[0] << " <path_to_hwloc_xml>\n";
-    return EXIT_FAILURE;
-  }
+    if (argc != 2) {
+        std::cerr << "usage: " << argv[0] << " <path_to_hwloc_xml>\n";
+        return EXIT_FAILURE;
+    }
 
-  sys_sage::Node node;
-  if (sys_sage::parseHwlocOutput(&node, argv[1]) != 0)
-    return EXIT_FAILURE;
+    sys_sage::Node node;
+    if (sys_sage::parseHwlocOutput(&node, argv[1]) != 0)
+        return EXIT_FAILURE;
 
-  size_t n = 1'000'000;
-  auto *a = new double[n];
-  auto *b = new double[n];
-  auto *c = new double[n];
-  double alpha = 3.14159;
+    size_t n = 1'000'000;
+    auto *a = new double[n];
+    auto *b = new double[n];
+    auto *c = new double[n];
+    double alpha = 3.14159;
 
-  int numCpus = get_nprocs();
-  int cpu = sched_getcpu();
-  int targetCpu = (cpu + 1) % numCpus;
-  int targetTargetCpu = (targetCpu + 1) % numCpus;
+    int numCpus = get_nprocs();
+    int cpu = sched_getcpu();
+    int targetCpu = (cpu + 1) % numCpus;
+    int targetTargetCpu = (targetCpu + 1) % numCpus;
 
-  PAPI_library_init(PAPI_VER_CURRENT);
+    PAPI_library_init(PAPI_VER_CURRENT);
 
-  int eventSet = PAPI_NULL;
-  PAPI_create_eventset(&eventSet);
-  PAPI_add_event(eventSet, PAPI_TOT_INS);
+    int eventSet = PAPI_NULL;
+    PAPI_create_eventset(&eventSet);
+    PAPI_add_event(eventSet, PAPI_TOT_INS);
 
-  sys_sage::Relation *metrics = nullptr;
-  sys_sage::SS_PAPI_start(eventSet, &metrics);
+    sys_sage::Relation *metrics = nullptr;
+    sys_sage::SS_PAPI_start(eventSet, &metrics);
 
-  saxpy(a, b, c, n, alpha);
-  sys_sage::SS_PAPI_read(metrics, &node);
-  migrate(targetCpu);
+    saxpy(a, b, c, n, alpha);
+    sys_sage::SS_PAPI_read(metrics, &node);
+    migrate(targetCpu);
 
-  saxpy(a, b, c, n, alpha);
-  sys_sage::SS_PAPI_read(metrics, &node);
-  migrate(targetTargetCpu);
+    saxpy(a, b, c, n, alpha);
+    sys_sage::SS_PAPI_read(metrics, &node);
+    migrate(targetTargetCpu);
 
-  saxpy(a, b, c, n, alpha);
-  sys_sage::SS_PAPI_read(metrics, &node);
-  migrate(cpu);
+    saxpy(a, b, c, n, alpha);
+    sys_sage::SS_PAPI_read(metrics, &node);
+    migrate(cpu);
 
-  saxpy(a, b, c, n, alpha);
-  sys_sage::SS_PAPI_read(metrics, &node);
+    saxpy(a, b, c, n, alpha);
+    sys_sage::SS_PAPI_read(metrics, &node);
 
-  PAPI_stop(eventSet, nullptr);
+    PAPI_stop(eventSet, nullptr);
 
-  assert(metrics->GetComponents().size() == 3);
+    assert(metrics->GetComponents().size() == 3);
 
-  // Depending on what is done above, assert certain properties and print
-  // results to validate certain assumptions. Assertions may fail sometimes,
-  // since behavior under thread migration is hard to predict with certainty.
+    // Depending on what is done above, assert certain properties and print
+    // results to validate certain assumptions. Assertions may fail sometimes,
+    // since behavior under thread migration is hard to predict with certainty.
 
-  const sys_sage::CpuMetrics *cpuMetrics = metrics->GetAllPAPImetrics(PAPI_TOT_INS, cpu);
-  assert(cpuMetrics != nullptr && cpuMetrics->entries.size() == 1);
-  std::cout << "CPU " << cpu << ": " << metrics->GetPAPImetric(PAPI_TOT_INS, cpu) << '\n';
+    const sys_sage::CpuMetrics *cpuMetrics = metrics->GetAllPAPImetrics(PAPI_TOT_INS, cpu);
+    assert(cpuMetrics != nullptr && cpuMetrics->entries.size() == 1);
+    std::cout << "CPU " << cpu << ": " << metrics->GetPAPImetric(PAPI_TOT_INS, cpu) << '\n';
 
-  const sys_sage::CpuMetrics *targetMetrics = metrics->GetAllPAPImetrics(PAPI_TOT_INS, targetCpu);
-  assert(targetMetrics != nullptr && targetMetrics->entries.size() == 1);
-  std::cout << "CPU " << targetCpu << ": " << metrics->GetPAPImetric(PAPI_TOT_INS, targetCpu) << '\n';
+    const sys_sage::CpuMetrics *targetMetrics = metrics->GetAllPAPImetrics(PAPI_TOT_INS, targetCpu);
+    assert(targetMetrics != nullptr && targetMetrics->entries.size() == 1);
+    std::cout << "CPU " << targetCpu << ": " << metrics->GetPAPImetric(PAPI_TOT_INS, targetCpu) << '\n';
 
-  const sys_sage::CpuMetrics *targetTargetMetrics = metrics->GetAllPAPImetrics(PAPI_TOT_INS, targetTargetCpu);
-  assert(targetTargetMetrics != nullptr && targetTargetMetrics->entries.size() == 1);
-  std::cout << "CPU " << targetTargetCpu << ": " << metrics->GetPAPImetric(PAPI_TOT_INS, targetTargetCpu) << '\n';
+    const sys_sage::CpuMetrics *targetTargetMetrics = metrics->GetAllPAPImetrics(PAPI_TOT_INS, targetTargetCpu);
+    assert(targetTargetMetrics != nullptr && targetTargetMetrics->entries.size() == 1);
+    std::cout << "CPU " << targetTargetCpu << ": " << metrics->GetPAPImetric(PAPI_TOT_INS, targetTargetCpu) << '\n';
 
-  std::cout << "\ntotal: " << metrics->GetPAPImetric(PAPI_TOT_INS) << '\n';
+    std::cout << "\ntotal: " << metrics->GetPAPImetric(PAPI_TOT_INS) << '\n';
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
--- examples/papi_monitor_process.cpp
+++ examples/papi_monitor_process.cpp
@@ -26,9 +26,9 @@
 #include <unistd.h>
 
 #define FATAL(errMsg, pid) do {\
-  std::cerr << "error: " << (errMsg) << '\n';\
-  kill(pid, SIGKILL);\
-  return EXIT_FAILURE;\
+    std::cerr << "error: " << (errMsg) << '\n';\
+    kill(pid, SIGKILL);\
+    return EXIT_FAILURE;\
 } while(false)
 
 const char *scriptContent = R"PY(
@@ -52,174 +52,174 @@ plt.close()
 
 int main(int argc, char **argv)
 {
-  if (argc < 3) {
-    std::cerr << "usage: " << argv[0] << " <path_to_hwloc_xml> <binary_to_execute> [params_for_binary]\n";
-    return EXIT_FAILURE;
-  }
-
-  sys_sage::Node node;
-  if (sys_sage::parseHwlocOutput(&node, argv[1]) != 0)
-    return EXIT_FAILURE;
-
-  pid_t pid = fork();
-  if (pid == -1) {
-    std::cerr << "error: " << strerror(errno) << '\n';
-    return EXIT_FAILURE;
-  } else if (pid == 0) {
-    ptrace(PTRACE_TRACEME, 0, nullptr, nullptr);
-    execvp(argv[2], argv + 2);
-    return EXIT_FAILURE;
-  }
-
-  int rval, status;
-
-  waitpid(pid, &status, 0);
-  ptrace(PTRACE_SETOPTIONS, pid, 0, PTRACE_O_TRACEEXIT);
-
-  rval = PAPI_library_init(PAPI_VER_CURRENT);
-  if (rval != PAPI_VER_CURRENT)
-    FATAL(PAPI_strerror(rval), pid);
-
-  int eventSet = PAPI_NULL;
-  rval = PAPI_create_eventset(&eventSet);
-  if (rval != PAPI_OK)
-    FATAL(PAPI_strerror(rval), pid);
-
-  int events[] = {
-    PAPI_TOT_INS,
-    PAPI_TOT_CYC
-  };
-  int numEvents = sizeof(events) / sizeof(events[0]);
-  rval = PAPI_add_events(eventSet, events, numEvents);
-  if (rval != PAPI_OK)
-    FATAL(PAPI_strerror(rval), pid);
-
-  // attach to child process
-  rval = PAPI_attach(eventSet, pid);
-  if (rval != PAPI_OK)
-    FATAL(PAPI_strerror(rval), pid);
-
-  sys_sage::Relation *metrics = nullptr;
-  rval = sys_sage::SS_PAPI_start(eventSet, &metrics);
-  if (rval != PAPI_OK)
-    FATAL(PAPI_strerror(rval), pid);
-
-  ptrace(PTRACE_CONT, pid, nullptr, nullptr);
-
-  // take repeated measurements
-  while (true) {
-    usleep(100);
-
-    waitpid(pid, &status, WNOHANG);
-    if (WIFSTOPPED(status) && (status >> 16) == PTRACE_EVENT_EXIT)
-      break;
-
-    rval = sys_sage::SS_PAPI_read(metrics, &node, true);
-    if (rval != PAPI_OK)
-      FATAL(PAPI_strerror(rval), pid);
-  }
-
-  ptrace(PTRACE_CONT, pid, nullptr, nullptr);
-
-  waitpid(pid, &status, 0);
-
-  rval = PAPI_stop(eventSet, nullptr);
-  if (rval != PAPI_OK) {
-    std::cerr << "error: " << PAPI_strerror(rval) << '\n';
-    return EXIT_FAILURE;
-  }
-
-  // plot the measurements taken on each CPU
-
-  std::filesystem::path plotDir = std::filesystem::weakly_canonical(std::filesystem::absolute(argv[0]).parent_path()) / "plots";
-  std::error_code err;
-  bool created = std::filesystem::create_directory(plotDir, err);
-  if (err) {
-    std::cerr << "error: could not create directory: " << err.message() << '\n';
-    return EXIT_FAILURE;
-  }
-
-  if (created) {
-    std::cout << "created directory " << plotDir << '\n';
-  } else {
-    for (const auto &file : std::filesystem::directory_iterator(plotDir))
-      std::filesystem::remove_all(file.path());
-  }
-
-  std::filesystem::path tmpScript = std::filesystem::temp_directory_path() / "plotScriptXXXXXX.py";
-  std::string tmpScriptName = tmpScript.string();
-  int fd = mkstemps(tmpScriptName.data(), 3);
-  if (fd == -1) {
-    std::cerr << "error: could not create " << tmpScriptName << '\n';
-    return EXIT_FAILURE;
-  }
-  close(fd);
-
-  {
-    std::ofstream script (tmpScriptName);
-    script << scriptContent;
-  }
-
-  // create a graph for each CPU
-  char buf[PAPI_MAX_STR_LEN];
-  for (auto cpu : metrics->GetComponents()) {
-    // create a timeline that starts relative to the start of the event set
-    std::ostringstream xVals;
-    std::ostringstream yVals;
-    std::ostringstream labels;
-
-    for (int i = 0; i < numEvents; i++) {
-      if (i > 0) {
-        xVals << ";";
-        yVals << ";";
-        labels << ";";
-      }
-
-      rval = PAPI_event_code_to_name(events[i], buf);
-      if (rval != PAPI_OK) {
-        std::cerr << "error: " << PAPI_strerror(rval) << '\n';
+    if (argc < 3) {
+        std::cerr << "usage: " << argv[0] << " <path_to_hwloc_xml> <binary_to_execute> [params_for_binary]\n";
         return EXIT_FAILURE;
-      }
-      labels << buf;
-
-      // get all collected metrics of the event on the CPU
-      auto cpuMetrics = metrics->GetAllPAPImetrics(events[i], cpu->GetId());
-      for (auto it = cpuMetrics->entries.begin(); it != cpuMetrics->entries.end(); it++) {
-        if (it != cpuMetrics->entries.begin()) {
-          xVals << ",";
-          yVals << ",";
-        }
-
-        xVals << metrics->GetElapsedTime(it->timestamp);
-        yVals << it->value;
-      }
     }
 
-    std::string cmd = "python3 " + tmpScriptName
-                    + " '" + (plotDir / ("CPU" + std::to_string(cpu->GetId()))).string()
-                    + "' '" + xVals.str()
-                    + "' '" + yVals.str()
-                    + "' '" + labels.str() + "'";
+    sys_sage::Node node;
+    if (sys_sage::parseHwlocOutput(&node, argv[1]) != 0)
+        return EXIT_FAILURE;
 
-    system(cmd.c_str());
-  }
+    pid_t pid = fork();
+    if (pid == -1) {
+        std::cerr << "error: " << strerror(errno) << '\n';
+        return EXIT_FAILURE;
+    } else if (pid == 0) {
+        ptrace(PTRACE_TRACEME, 0, nullptr, nullptr);
+        execvp(argv[2], argv + 2);
+        return EXIT_FAILURE;
+    }
 
-  std::cout << "plots written to " << plotDir << '\n';
-  std::filesystem::remove(tmpScript);
+    int rval, status;
 
-  rval = PAPI_cleanup_eventset(eventSet);
-  if (rval != PAPI_OK) {
-    std::cerr << "error: " << PAPI_strerror(rval) << '\n';
-    return EXIT_FAILURE;
-  }
+    waitpid(pid, &status, 0);
+    ptrace(PTRACE_SETOPTIONS, pid, 0, PTRACE_O_TRACEEXIT);
+
+    rval = PAPI_library_init(PAPI_VER_CURRENT);
+    if (rval != PAPI_VER_CURRENT)
+        FATAL(PAPI_strerror(rval), pid);
+
+    int eventSet = PAPI_NULL;
+    rval = PAPI_create_eventset(&eventSet);
+    if (rval != PAPI_OK)
+        FATAL(PAPI_strerror(rval), pid);
+
+    int events[] = {
+        PAPI_TOT_INS,
+        PAPI_TOT_CYC
+    };
+    int numEvents = sizeof(events) / sizeof(events[0]);
+    rval = PAPI_add_events(eventSet, events, numEvents);
+    if (rval != PAPI_OK)
+        FATAL(PAPI_strerror(rval), pid);
+
+    // attach to child process
+    rval = PAPI_attach(eventSet, pid);
+    if (rval != PAPI_OK)
+        FATAL(PAPI_strerror(rval), pid);
+
+    sys_sage::Relation *metrics = nullptr;
+    rval = sys_sage::SS_PAPI_start(eventSet, &metrics);
+    if (rval != PAPI_OK)
+        FATAL(PAPI_strerror(rval), pid);
+
+    ptrace(PTRACE_CONT, pid, nullptr, nullptr);
+
+    // take repeated measurements
+    while (true) {
+        usleep(100);
+
+        waitpid(pid, &status, WNOHANG);
+        if (WIFSTOPPED(status) && (status >> 16) == PTRACE_EVENT_EXIT)
+            break;
+
+        rval = sys_sage::SS_PAPI_read(metrics, &node, true);
+        if (rval != PAPI_OK)
+            FATAL(PAPI_strerror(rval), pid);
+    }
+
+    ptrace(PTRACE_CONT, pid, nullptr, nullptr);
+
+    waitpid(pid, &status, 0);
+
+    rval = PAPI_stop(eventSet, nullptr);
+    if (rval != PAPI_OK) {
+        std::cerr << "error: " << PAPI_strerror(rval) << '\n';
+        return EXIT_FAILURE;
+    }
+
+    // plot the measurements taken on each CPU
+
+    std::filesystem::path plotDir = std::filesystem::weakly_canonical(std::filesystem::absolute(argv[0]).parent_path()) / "plots";
+    std::error_code err;
+    bool created = std::filesystem::create_directory(plotDir, err);
+    if (err) {
+        std::cerr << "error: could not create directory: " << err.message() << '\n';
+        return EXIT_FAILURE;
+    }
+
+    if (created) {
+        std::cout << "created directory " << plotDir << '\n';
+    } else {
+        for (const auto &file : std::filesystem::directory_iterator(plotDir))
+            std::filesystem::remove_all(file.path());
+    }
+
+    std::filesystem::path tmpScript = std::filesystem::temp_directory_path() / "plotScriptXXXXXX.py";
+    std::string tmpScriptName = tmpScript.string();
+    int fd = mkstemps(tmpScriptName.data(), 3);
+    if (fd == -1) {
+        std::cerr << "error: could not create " << tmpScriptName << '\n';
+        return EXIT_FAILURE;
+    }
+    close(fd);
+
+    {
+        std::ofstream script (tmpScriptName);
+        script << scriptContent;
+    }
+
+    // create a graph for each CPU
+    char buf[PAPI_MAX_STR_LEN];
+    for (auto cpu : metrics->GetComponents()) {
+        // create a timeline that starts relative to the start of the event set
+        std::ostringstream xVals;
+        std::ostringstream yVals;
+        std::ostringstream labels;
+
+        for (int i = 0; i < numEvents; i++) {
+            if (i > 0) {
+                xVals << ";";
+                yVals << ";";
+                labels << ";";
+            }
+
+            rval = PAPI_event_code_to_name(events[i], buf);
+            if (rval != PAPI_OK) {
+                std::cerr << "error: " << PAPI_strerror(rval) << '\n';
+                return EXIT_FAILURE;
+            }
+            labels << buf;
+
+            // get all collected metrics of the event on the CPU
+            auto cpuMetrics = metrics->GetAllPAPImetrics(events[i], cpu->GetId());
+            for (auto it = cpuMetrics->entries.begin(); it != cpuMetrics->entries.end(); it++) {
+                if (it != cpuMetrics->entries.begin()) {
+                    xVals << ",";
+                    yVals << ",";
+                }
+
+                xVals << metrics->GetElapsedTime(it->timestamp);
+                yVals << it->value;
+            }
+        }
+
+        std::string cmd = "python3 " + tmpScriptName
+                        + " '" + (plotDir / ("CPU" + std::to_string(cpu->GetId()))).string()
+                        + "' '" + xVals.str()
+                        + "' '" + yVals.str()
+                        + "' '" + labels.str() + "'";
+
+        system(cmd.c_str());
+    }
+
+    std::cout << "plots written to " << plotDir << '\n';
+    std::filesystem::remove(tmpScript);
+
+    rval = PAPI_cleanup_eventset(eventSet);
+    if (rval != PAPI_OK) {
+        std::cerr << "error: " << PAPI_strerror(rval) << '\n';
+        return EXIT_FAILURE;
+    }
  
-  rval = PAPI_destroy_eventset(&eventSet);
-  if (rval != PAPI_OK) {
-    std::cerr << "error: " << PAPI_strerror(rval) << '\n';
-    return EXIT_FAILURE;
-  }
+    rval = PAPI_destroy_eventset(&eventSet);
+    if (rval != PAPI_OK) {
+        std::cerr << "error: " << PAPI_strerror(rval) << '\n';
+        return EXIT_FAILURE;
+    }
 
-  PAPI_shutdown();
+    PAPI_shutdown();
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
